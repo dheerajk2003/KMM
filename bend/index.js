6a03b111c41ed4e795e74eda09d7864059e9da22
Dheeeraj Khatri <dheerajkhatri@gmail.com>
2023-11-29 16:54:42 +0530

    temp

--- bend/index.js
+++ bend/index.js
@@ -76,7 +76,7 @@ app.get('*.js', (req, res, next) => {
   next();
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname,'./','dist','index.html'));
 });
 
